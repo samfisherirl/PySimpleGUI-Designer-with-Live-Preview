f8914605c594c4b3b5efb8aee33e73d493e013d8
Sasha Alexander <98753696+samfisherirl@users.noreply.github.com>
2023-03-18 09:02:09 -0500

    Add files via upload

--- button_functions.py
+++ button_functions.py
@@ -1,23 +1,17 @@
 '''these are the functions that will be used in the main function, each function is a button on the GUI'''
-
 class Elements:
     def __init__(self):
-        self.main = []
-        self.container = []
-        self.design = []
-        self.organization = []
-        self.info = []
-        self.drawing = []
-        self.other = []
-        self.layouts = []
+        self.layout_ = []
+        self.key = ''
 
-    def layout_append(self, obj_name, value):
-        if hasattr(self, obj_name):
-            obj = getattr(self, obj_name)
-            obj.append(value)
+    def layout_append(self, *args):
+        try:
+            self.layout_.append(args)
+        except:
+            pass
 
     def return_layout(self):
-        return self.layouts
+        return [self.layout_]
 
     def return_list(self, obj_name):
         obj = getattr(self, obj_name)
@@ -27,49 +21,7 @@ def layout(sg, make_button_row):
     Ele = Elements()
     # Makes a button for each string in the given list of strings
      
-    layout = []     
-    Ele.layout_append('layouts',[
-        sg.Button(button_text="Delete Element",
-                  button_color=("black", "darkred"),
-                  key="DeleteElement"),
-        sg.Button('Move Element Up ˄', key="MoveUp"),
-        sg.Button('Move Element Down ˅', key="MoveDown"),
-        sg.Button('~Theme~', key="Theme")
-    ])
-
-    Ele.layout_append('layouts', [sg.Text(text="Main Elements")])
-    Ele.layout_append('layouts', make_button_row(Ele.main))
-
-    Ele.layout_append('layouts', [sg.Text(text="Containers")])
-    Ele.container = ["TabGroup", "Tab", "Frame", "Column"]
-    Ele.layout_append('layouts', make_button_row(Ele.container))
-
-    Ele.layout_append('layouts', [sg.Text(text="Separators")])
-    Ele.design = ["HorizontalSeparator", "VerticalSeparator"]
-    Ele.layout_append('layouts', make_button_row(Ele.design))
-
-    Ele.layout_append('layouts', [sg.Text(text="Menus")])
-    Ele.organization = ["Menu", "MenuButton", "OptionMenu", "ButtonMenu"]
-    Ele.layout_append('layouts', make_button_row(Ele.organization))
-
-    Ele.layout_append('layouts', [sg.Text(text="Structured Info")])
-    Ele.info = ["Tree", "Table"]
-    Ele.layout_append('layouts', make_button_row(Ele.info))
-
-    Ele.layout_append('layouts', [sg.Text(text="Images/Drawing")])
-    Ele.drawing = ["Image", "Graph", "Canvas"]
-    Ele.layout_append('layouts', make_button_row(Ele.drawing))
-
-    Ele.layout_append('layouts', [sg.Text(text="Others")])
-    Ele.others = [
-        "Slider", "Spin", "Multiline", "ProgressBar", "StatusBar"
-    ]
-    Ele.layout_append('layouts', layout)
-
-    for i in layout:
-        Ele.layout_append('layouts', i)
-    return Ele
-
+    layout = []      
 
 
 def get_theme():
